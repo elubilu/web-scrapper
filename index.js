3bf06ccb08458c1257e15c528511ef8095262470
Rasel Ahmed <raselahmed.lucse@gmail.com>
2022-12-09 10:21:45 +0000

    Added Workflow automation

--- index.js
+++ index.js
@@ -8,70 +8,7 @@ puppeteer.use(StealthPlugin())
 
 
 
-// puppeteer usage as normal
-puppeteer.launch({ headless: true }).then(async browser => {
-    console.log('Running tests..')
-    try {
-    //     fsC.createReadStream("patentList/ListOfPatent2.csv")
-    //     .pipe(csvParser())
-    //     .on('data', async function(data){
-            // try {
-            //     console.log("Link is: "+data.CitedLink);
-            //     console.log("Patent Number is: "+data.PatentNo);
-            //     const page = await browser.newPage();
-            //     await page.goto(data.CitedLink);
-            //     await page.waitForSelector('.btn.btn-xs.analysis-toolbar-item.ng-binding');
-            //     await page.evaluate(() => {
-            //         document.querySelectorAll(".btn.btn-xs.analysis-toolbar-item.ng-binding")[3].click();
-            //     });
-            //     await page.waitForSelector('[name="filename"]');
-            //     await page.evaluate(() => {
-            //         document.querySelector('[name="filename"]').value = data.PatentNo;
-            //     });
-            //     await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './myAwesomeDownloadFolder'});
-            //     await page.waitForSelector('.btn.btn-sm.btn-pri.ng-binding');
-            //     await page.evaluate(() => {
-            //         document.querySelector(".btn.btn-sm.btn-pri.ng-binding").click();
-            //     });
-            //     // await page.waitFor(Math.floor(Math.random() * (15000 - 5000 + 1)) + 5000)
-            // }
-            // catch(err) {
-            //     //error handler
-            // }
-        // })
-        // .on('end',async function(){
-            //some final operations
-                const page = await browser.newPage();
-                await page.goto("https://www.lens.org/lens/search/patent/list?q=reference_cited.patent.lens_id:068-887-823-650-053");
-                await page.waitFor(5000);
-                await page.waitForSelector('.btn.btn-xs.analysis-toolbar-item.ng-binding');
-                await page.evaluate(() => {
-                    document.querySelectorAll(".btn.btn-xs.analysis-toolbar-item.ng-binding")[3].click();
-                });
-                await page.waitForSelector('[name="filename"]');
-                await page.evaluate(() => {
-                    document.querySelector('[name="filename"]').value = "test";
-                });
-                await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './myAwesomeDownloadFolder'});
-                await page.waitForSelector('.btn.btn-sm.btn-pri.ng-binding');
-                await page.evaluate(() => {
-                    document.querySelector(".btn.btn-sm.btn-pri.ng-binding").click();
-                });
-                await page.waitFor(Math.floor(Math.random() * (15000 - 5000 + 1)) + 5000)
-        // });  
-        
-       
-        // await page.screenshot({ path: new Date().getTime().toString()+".png"});
-        console.log('Ended test..')
-    } catch (e) {
-        console.log(e)
-    }
 
-
-
-//   await browser.close()
-  console.log(`All done, check the file ✨`)
-})
 
 module.exports.singleUrl = (url)=>{
     puppeteer.launch({ headless: true }).then(async browser => {
@@ -104,4 +41,71 @@ module.exports.singleUrl = (url)=>{
       await browser.close()
       console.log(`All done, check the file ✨`)
     })
+}
+
+module.exports.listOfUrl = (csvURL) => {
+    // puppeteer usage as normal
+puppeteer.launch({ headless: true }).then(async browser => {
+    console.log('Running tests..')
+    try {
+        fsC.createReadStream(csvURL)
+        .pipe(csvParser())
+        .on('data', async function(data){
+            try {
+                console.log("Link is: "+data.CitedLink);
+                console.log("Patent Number is: "+data.PatentNo);
+                const page = await browser.newPage();
+                await page.goto(data.CitedLink);
+                await page.waitForSelector('.btn.btn-xs.analysis-toolbar-item.ng-binding');
+                await page.evaluate(() => {
+                    document.querySelectorAll(".btn.btn-xs.analysis-toolbar-item.ng-binding")[3].click();
+                });
+                await page.waitForSelector('[name="filename"]');
+                await page.evaluate(() => {
+                    document.querySelector('[name="filename"]').value = data.PatentNo;
+                });
+                await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './myAwesomeDownloadFolder'});
+                await page.waitForSelector('.btn.btn-sm.btn-pri.ng-binding');
+                await page.evaluate(() => {
+                    document.querySelector(".btn.btn-sm.btn-pri.ng-binding").click();
+                });
+                // await page.waitFor(Math.floor(Math.random() * (15000 - 5000 + 1)) + 5000)
+            }
+            catch(err) {
+                //error handler
+            }
+        })
+        .on('end',async function(){
+            //some final operations
+                const page = await browser.newPage();
+                await page.goto("https://www.lens.org/lens/search/patent/list?q=reference_cited.patent.lens_id:068-887-823-650-053");
+                await page.waitFor(5000);
+                await page.waitForSelector('.btn.btn-xs.analysis-toolbar-item.ng-binding');
+                await page.evaluate(() => {
+                    document.querySelectorAll(".btn.btn-xs.analysis-toolbar-item.ng-binding")[3].click();
+                });
+                await page.waitForSelector('[name="filename"]');
+                await page.evaluate(() => {
+                    document.querySelector('[name="filename"]').value = "test";
+                });
+                await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: './myAwesomeDownloadFolder'});
+                await page.waitForSelector('.btn.btn-sm.btn-pri.ng-binding');
+                await page.evaluate(() => {
+                    document.querySelector(".btn.btn-sm.btn-pri.ng-binding").click();
+                });
+                await page.waitFor(Math.floor(Math.random() * (15000 - 5000 + 1)) + 5000)
+        });  
+        
+       
+        // await page.screenshot({ path: new Date().getTime().toString()+".png"});
+        console.log('Ended test..')
+    } catch (e) {
+        console.log(e)
+    }
+
+
+
+//   await browser.close()
+  console.log(`All done, check the file ✨`)
+})
 }
